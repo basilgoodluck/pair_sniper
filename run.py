8da006c4e2e497a5c5c00a501cd5dd15a73abd96
Noble <basilgoodluck22@gmail.com>
2025-05-07 09:14:20 +0100

    added requirements.txt and test api

---
+++ run.py
@@ -0,0 +1,11 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv("TWELVE_DATA_TOKEN")
+
+response = requests.get(f"https://api.twelvedata.com/time_series?symbol=EUR/USD&interval=1day&apikey={API_TOKEN}")
+
+print(response.json())
